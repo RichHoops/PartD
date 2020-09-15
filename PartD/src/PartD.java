ad381f7bb07103bf6cf49209c7fe2fcde6dd8d30
RichHoops <RichardHooper@myop.ac.nz>
2020-09-15 21:49:28 +1200

    Adding second line, favourite colour.

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -2,7 +2,10 @@
 public class PartD {
 
 	public static void main(String[] args) {
+		//First line stating name
 		System.out.println("Hi, my name is Richard.");
+		//Second line stating favourite colour
+		System.out.println("My favourite colour is green.");
 
 	}
 
