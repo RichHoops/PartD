a444b85bc7290c2ab151950858869b3ff5e771d5
RichHoops <RichardHooper@myop.ac.nz>
2020-09-15 21:51:41 +1200

    Added third line, favourite mode of transport.

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -6,6 +6,8 @@ public class PartD {
 		System.out.println("Hi, my name is Richard.");
 		//Second line stating favourite colour
 		System.out.println("My favourite colour is green.");
+		//Third line stating favourite mode of transport
+		System.out.println("My favourite mode of transport is motorcycle.");
 
 	}
 
